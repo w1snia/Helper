3376f173cab4165dbdd2a8b88b256420cf9b55af
Michał Wiśniewski <michal.wisnia23@gmail.com>
2020-12-08 21:49:56 +0100

    Add project files.

---
+++ Helper/Models/AppDbContext/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Helper.Models.AppDbContext
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ Helper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Helper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
