3c2e03494bcacdaf3b7a8ae1c29cc8749252d114
Michał Wiśniewski <michal.wisnia23@gmail.com>
2020-12-09 10:47:42 +0100

    add PersonController and view - List

---
+++ Helper/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Helper.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Helper.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly IPersonRepository _personRepository;
+
+        public PersonController(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(_personRepository.GetPersons());
+        }
+
+
+    }
+}
