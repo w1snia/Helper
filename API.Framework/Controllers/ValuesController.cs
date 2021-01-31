4cc6842d8d1d0784eeb63e1eeabdcd94855c700b
Michał Wiśniewski <michal.wisnia23@gmail.com>
2021-01-31 19:25:50 +0100

    dodanie projektu API .NET CORE

--- API.Framework/Controllers/ValuesController.cs
+++ API.Framework/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace API.Framework.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return $"value {id}";
         }
 
         // POST api/values
